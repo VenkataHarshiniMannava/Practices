501c4b3f26c83f2f2a5da02370f110cd5f0686cd
VenkataHarshiniMannava <115424936+VenkataHarshiniMannava@users.noreply.github.com>
2023-08-03 19:51:25 +0530

    practice reactjs

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ExpenseList from './components/ExpenseList';
+
+const App = () => {
+  return (
+    <div>
+      <h1>My Expenses</h1>
+      <ExpenseList />
+    </div>
+  );
+};
+
+export default App;
